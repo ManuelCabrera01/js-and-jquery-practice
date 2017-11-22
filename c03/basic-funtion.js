32f26b5f237522992d8b90ecab6119f6d7190e60
Manuel <m.a.c.p90@gmail.com>
2017-11-22 12:38:01 -0500

    change message in html

---
+++ c03/basic-funtion.js
@@ -0,0 +1,7 @@
+var msg = 'sign up to reciver our news letter for 10% off';
+
+function updAteMassage (){
+  var el = document.getElementById('message');
+  el.textContent= msg;
+}
+updAteMassage();
