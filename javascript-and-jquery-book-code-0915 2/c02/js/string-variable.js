625534417c7522d99421dfa117931cedf7b50c7f
Manuel <m.a.c.p90@gmail.com>
2018-02-27 09:03:42 -0500

    second lesson finish

---
+++ javascript-and-jquery-book-code-0915 2/c02/js/string-variable.js
@@ -0,0 +1,8 @@
+var username;
+var message;
+username = "Molly";
+message = 'see our upcomming range';
+
+var elName = document.getElementById('name');
+var elNote = document.getElementById('note');
+elNote.textContent = message;
---
+++ javascript-and-jquery-book-code-0915 2/c02/js/string-with-quotes.js
@@ -0,0 +1,9 @@
+var tittle;
+var message;
+tittle = "Molly's spetial offers";
+message = " <a href = \" sale.html\" > 25% off! </a>";
+
+var elTittle= document.getElementById('tittle');
+elTittle.textContent= tittle;
+var elNote = document.getElementById('note');
+elNote.textContent = message;
