fb23bc368dee2c5608c439cbe25c56d7c1746249
Manuel <m.a.c.p90@gmail.com>
2018-03-11 10:26:11 -0400

    sobling and child selector

---
+++ javascript-and-jquery-book-code-0915 2/c05/js/child.js
@@ -0,0 +1,6 @@
+var startItem = document.getElementByTagName('ul')[0];
+var firstItem = startItem.firstChild;
+var lastItem = startItem.lastChild;
+
+firstItem.selectAttribute('class', 'complerte');
+lastItem.selectAttribute('class', 'cool');
