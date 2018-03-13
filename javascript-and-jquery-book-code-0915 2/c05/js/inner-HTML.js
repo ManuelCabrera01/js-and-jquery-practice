d766c4054ba0010c6e9016a6d9543a7e542f9ee7
Manuel <m.a.c.p90@gmail.com>
2018-03-13 11:28:57 -0400

    iner html sample

---
+++ javascript-and-jquery-book-code-0915 2/c05/js/inner-HTML.js
@@ -0,0 +1,6 @@
+var firstItem = document.getElementById('one');
+
+var itemContent = firstItem.innerHTML;
+
+ firstItem.innetHTML = '<a href=\"http://example.org\">'+itemContent+ '</a>';
+ 
--- javascript-and-jquery-book-code-0915 2/c05/js/node-value.js
+++ javascript-and-jquery-book-code-0915 2/c05/js/node-value.js
@@ -5,3 +5,4 @@ var elText = itemTwo.firstChild.nodeValue;
 elText = elText.replace('pine nuts ' , 'kale');
 
 itemTwo.fisrtChild.nodeValue = elText;
+ 
