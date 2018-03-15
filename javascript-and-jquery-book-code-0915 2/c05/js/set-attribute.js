3fc800e44ee9cc4b21ba3975a2a22e575a7a109f
Manuel <m.a.c.p90@gmail.com>
2018-03-15 11:31:07 -0400

    set attribute. js

---
+++ javascript-and-jquery-book-code-0915 2/c05/js/set-attribute.js
@@ -0,0 +1,5 @@
+var firstItem = document.getElementById('one');
+firstItem.className = 'complete';
+
+var fouthItem= document.getElementByTagName('li').item(3);
+fouthItem.selectAttribute('class', 'cool');
