856d6373af9e9b20dc279be3de3ab0521685ef0e
Manuel <m.a.c.p90@gmail.com>
2018-03-15 13:08:31 -0400

    using innerHTML to select and update headding ?DOM

--- javascript-and-jquery-book-code-0915 2/c05/js/example.js
+++ javascript-and-jquery-book-code-0915 2/c05/js/example.js
@@ -8,3 +8,16 @@ var newTextLast = createTextNode('cream');
  var newTextFirst= createTextNode('kale');
  newItemFirst.appendChild('newTextFirst');
  list.insetBefore(newItemFirst, list.fisrtChild);
+
+
+ var listItems = document.querySelectorAll('li');
+
+ var i ;
+for (i =0; i< lastItem.length; i ++){
+  listItems[i].className = "cool";
+}
+var heading = document.querySelector('h2');
+var hedingText = heading.firstChild.nodeValue;
+var totalItems = listItems.lenght;
+var newHeading = headingText +' <span'> + totalItems + '</span>' ;
+heading.innerHTML = newHeading;
