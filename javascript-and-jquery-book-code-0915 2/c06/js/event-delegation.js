197c7d9f0892b9c62d53ff41fe3a66f504de7639
Manuel <m.a.c.p90@gmail.com>
2018-03-23 11:36:44 -0400

    event-delegation .js

--- javascript-and-jquery-book-code-0915 2/c06/js/event-delegation.js
+++ javascript-and-jquery-book-code-0915 2/c06/js/event-delegation.js
@@ -1,3 +1,31 @@
-funtion(){
-  
+function getTarget(e){
+  if(!e){
+    e = window.event;
+  }
+  return e.target || e.srcElement;
+}
+
+function itemDone(e){
+  var targuet , elParent, elGranparent;
+  targuet = getTarget(e);
+  elParent = targuet.parentNode;
+  elGranparent =targuet.parentNode.parentNode;
+  elGranparent.removeChild(elParent);
+
+  if (e.preventDefault){
+    e.preventDefault();
+
+  }else{
+    e.returnValue = false;
+  }
+}
+var el = document.getElementById('shoppingList');
+if (e.addEventListener){
+  e.addEventListener('click', function(e){
+    itemDone(e);
+  },  false);
+}else{
+  el.attachEvent('onclick', function(e){
+    itemDone(e);
+  });
 }
